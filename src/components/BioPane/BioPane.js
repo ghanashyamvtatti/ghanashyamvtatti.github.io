e74286873df4df81e99ab9c25262c82a2f8ff1b2
Ghanashyam <ghanashyamvtatti@gmail.com>
2019-09-02 19:15:06 -0400

    Replace placeholder bio with actual bio

--- src/components/BioPane/BioPane.js
+++ src/components/BioPane/BioPane.js
@@ -6,14 +6,9 @@ import './BioPane.css';
 function BioPane() {
     return (
         <Pane className="bio-pane">
-        <Bio bio="My name is Wally West and I am the fastest man alive!
-            My name is Wally West and I am the fastest man alive!
-            My name is Wally West and I am the fastest man alive! 
-            My name is Wally West and I am the fastest man alive! 
-            My name is Wally West and I am the fastest man alive! 
-            My name is Wally West and I am the fastest man alive! 
-            My name is Wally West and I am the fastest man alive! "/>
-        
+        <Bio bio="I'm a former Machine Learning Engineer (although I've worked full-stack throughout my career). \n
+        My interests lie in Computer Vision (image-classification, image-segmentation), Generative Adversarial Networks, and Natural Language Processing.\n
+        In my free time, I game and binge-watch anime."/>
       </Pane>
     );
 }
