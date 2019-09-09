a70900417172595f64d799f22a795a475e0f68e1
Ghanashyam <ghanashyamvtatti@gmail.com>
2019-09-08 20:39:59 -0400

    First page deployment

---
+++ precache-manifest.aef2eab511272c70acc05b1b009ceb5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de67531421baa56174fb9b3e2bd885f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc462b7f3457ad0531a",
+    "url": "/static/css/main.1fe7dbb6.chunk.css"
+  },
+  {
+    "revision": "8c027bc161cd8d3d3cfc",
+    "url": "/static/js/2.663ec657.chunk.js"
+  },
+  {
+    "revision": "fbc462b7f3457ad0531a",
+    "url": "/static/js/main.03745d9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
