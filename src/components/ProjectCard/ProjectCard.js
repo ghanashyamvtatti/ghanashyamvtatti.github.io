414e84ca1e3c8b146767be4bab1176c44f40d5e6
Ghanashyam <ghanashyamvtatti@gmail.com>
2019-09-08 20:26:35 -0400

    Add description to project cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,11 +1,14 @@
 import { Card, Heading } from 'evergreen-ui';
 import React from 'react';
 import './ProjectCard.css';
+import { Pane } from 'evergreen-ui/commonjs/layers';
 
 function ProjectCard(props) {
+    let newDesc = props.desc ? props.desc.split('\\n').map((item, i) => <p key={i}>{item}</p>): '';
     return (
         <Card elevation={1} className="card">
-            <Heading size={700} margin-top={30} className="project-name">{props.projectName}</Heading>
+            <Heading size={700} className="project-name">{props.projectName}</Heading>
+            <Pane className="short-desc">{newDesc}</Pane>
         </Card>
     );
 }
