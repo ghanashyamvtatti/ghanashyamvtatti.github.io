663261f1e69cbe7ab2fb69856a208201cbb12b57
Ghanashyam <ghanashyamvtatti@gmail.com>
2020-01-16 21:14:29 -0500

    Load projects from firebase

---
+++ precache-manifest.35cbf74f8ba35940753f178f82589f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebee2f59c57b7849c069080bc6bb16c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928d2cf76532a8bf6449",
+    "url": "/static/css/main.a29ad26a.chunk.css"
+  },
+  {
+    "revision": "207f0d2749e6a6f9da84",
+    "url": "/static/js/2.62d88873.chunk.js"
+  },
+  {
+    "revision": "928d2cf76532a8bf6449",
+    "url": "/static/js/main.e41da2c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5372cc1ec8b7bfcd61c41f6e1a4698e1.js"
+  "/precache-manifest.35cbf74f8ba35940753f178f82589f3e.js"
 );
 
 self.addEventListener('message', (event) => {
